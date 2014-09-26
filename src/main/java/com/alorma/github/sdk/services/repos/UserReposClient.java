d8d12a4c9dc8a3bcbdb19e716b9f33d174756d52
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-09-26 19:29:33 +0200

    Modified repositories client

--- src/main/java/com/alorma/github/sdk/services/repos/UserReposClient.java
+++ src/main/java/com/alorma/github/sdk/services/repos/UserReposClient.java
@@ -4,35 +4,35 @@ import android.content.Context;
 
 public class UserReposClient extends BaseReposClient {
 
-    public UserReposClient(Context context) {
-        super(context);
-    }
+	public UserReposClient(Context context) {
+		super(context);
+	}
 
-    public UserReposClient(Context context, String username) {
-        super(context, username);
-    }
+	public UserReposClient(Context context, String username) {
+		super(context, username);
+	}
 
-    public UserReposClient(Context context, String username, int page) {
-        super(context, username, page);
-    }
+	public UserReposClient(Context context, String username, int page) {
+		super(context, username, page);
+	}
 
-    @Override
-    protected void executeUserFirstPage(String sort, ReposService usersService) {
-        usersService.userReposList(this);
-    }
+	@Override
+	protected void executeUserFirstPage(String sort, ReposService usersService) {
+		usersService.userReposList(sort, this);
+	}
 
-    @Override
-    protected void executeFirstPageByUsername(String username, String sort, ReposService usersService) {
-        usersService.userReposList(username, this);
-    }
+	@Override
+	protected void executeFirstPageByUsername(String username, String sort, ReposService usersService) {
+		usersService.userReposList(username, sort, this);
+	}
 
-    @Override
-    protected void executeUserPaginated(int page, String sort, ReposService usersService) {
-        usersService.userReposList(page, this);
-    }
+	@Override
+	protected void executeUserPaginated(int page, String sort, ReposService usersService) {
+		usersService.userReposList(page, sort, this);
+	}
 
-    @Override
-    protected void executePaginatedByUsername(String username, int page, String sort, ReposService usersService) {
-        usersService.userReposList(username, page, this);
-    }
+	@Override
+	protected void executePaginatedByUsername(String username, int page, String sort, ReposService usersService) {
+		usersService.userReposList(username, page, sort, this);
+	}
 }
--- src/main/java/com/alorma/github/sdk/services/repos/WatchedReposClient.java
+++ src/main/java/com/alorma/github/sdk/services/repos/WatchedReposClient.java
@@ -5,39 +5,39 @@ import android.content.Context;
 /**
  * Created by Bernat on 17/07/2014.
  */
-public class WatchedReposClient extends BaseReposClient{
+public class WatchedReposClient extends BaseReposClient {
 
-    public WatchedReposClient(Context context) {
-        super(context);
-    }
+	public WatchedReposClient(Context context) {
+		super(context);
+	}
 
-    public WatchedReposClient(Context context, String username) {
-        super(context, username);
-    }
+	public WatchedReposClient(Context context, String username) {
+		super(context, username);
+	}
 
-    public WatchedReposClient(Context context, String username, int page) {
-        super(context, username, page);
-    }
+	public WatchedReposClient(Context context, String username, int page) {
+		super(context, username, page);
+	}
 
-    @Override
-    protected void executeUserFirstPage(String sort, ReposService usersService) {
-        usersService.userSubscribedReposList(this);
-    }
+	@Override
+	protected void executeUserFirstPage(String sort, ReposService usersService) {
+		usersService.userSubscribedReposList(sort, this);
+	}
 
-    @Override
-    protected void executeFirstPageByUsername(String username, String sort, ReposService usersService) {
-        usersService.userSubscribedReposList(username, this);
-    }
+	@Override
+	protected void executeFirstPageByUsername(String username, String sort, ReposService usersService) {
+		usersService.userSubscribedReposList(username, sort, this);
+	}
 
-    @Override
-    protected void executeUserPaginated(int page, String sort, ReposService usersService) {
-        usersService.userSubscribedReposList(page, this);
-    }
+	@Override
+	protected void executeUserPaginated(int page, String sort, ReposService usersService) {
+		usersService.userSubscribedReposList(page, sort, this);
+	}
 
-    @Override
-    protected void executePaginatedByUsername(String username, int page, String sort, ReposService usersService) {
-        usersService.userSubscribedReposList(username, page, this);
-    }
+	@Override
+	protected void executePaginatedByUsername(String username, int page, String sort, ReposService usersService) {
+		usersService.userSubscribedReposList(username, page, sort, this);
+	}
 
 
 }
