197d079c4f8562bb5d92fcba103786efcf1359a7
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-10-05 13:29:15 +0200

    Watch event payload

---
+++ src/main/java/com/alorma/github/sdk/bean/dto/response/events/payload/WatchedEventPayload.java
@@ -0,0 +1,8 @@
+package com.alorma.github.sdk.bean.dto.response.events.payload;
+
+/**
+ * Created by Bernat on 04/10/2014.
+ */
+public class WatchedEventPayload extends GithubEventPayload {
+	public String action;
+}
