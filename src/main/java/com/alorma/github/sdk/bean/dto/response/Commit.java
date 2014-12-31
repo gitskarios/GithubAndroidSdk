261b83d704385d3f9acb927960fcb343f6dcb913
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-12-31 21:52:25 +0100

    Add days object in Commit clas, helper to headers list

--- src/main/java/com/alorma/github/sdk/bean/dto/response/Commit.java
+++ src/main/java/com/alorma/github/sdk/bean/dto/response/Commit.java
@@ -10,6 +10,7 @@ public class Commit extends ShaUrl{
 	public String message;
 	public boolean distinct;
 	public GitCommitFiles files;
+	public long days;
 
 	@Override
 	public String toString() {
