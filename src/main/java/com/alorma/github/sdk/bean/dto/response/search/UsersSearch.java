bac615c2dbe0c835529ce99ca1c9d7f4d13d6b42
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-02-01 19:23:47 +0100

    Fix user search

--- src/main/java/com/alorma/github/sdk/bean/dto/response/search/UsersSearch.java
+++ src/main/java/com/alorma/github/sdk/bean/dto/response/search/UsersSearch.java
@@ -1,5 +1,6 @@
 package com.alorma.github.sdk.bean.dto.response.search;
 
+import com.alorma.github.sdk.bean.dto.response.ListUsers;
 import com.alorma.github.sdk.bean.dto.response.User;
 
 import java.util.List;
@@ -8,5 +9,5 @@ import java.util.List;
  * Created by Bernat on 08/08/2014.
  */
 public class UsersSearch extends SearchBase {
-    public List<User> items;
+    public ListUsers items;
 }
--- src/main/java/com/alorma/github/sdk/services/search/UsersSearchClient.java
+++ src/main/java/com/alorma/github/sdk/services/search/UsersSearchClient.java
@@ -2,33 +2,58 @@ package com.alorma.github.sdk.services.search;
 
 import android.content.Context;
 
+import com.alorma.github.sdk.bean.dto.response.ListRepos;
+import com.alorma.github.sdk.bean.dto.response.ListUsers;
 import com.alorma.github.sdk.bean.dto.response.search.ReposSearch;
 import com.alorma.github.sdk.bean.dto.response.search.UsersSearch;
 import com.alorma.github.sdk.services.client.BaseClient;
 
+import retrofit.Callback;
 import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 /**
  * Created by Bernat on 08/08/2014.
  */
-public class UsersSearchClient extends BaseSearchClient<UsersSearch> {
+public class UsersSearchClient extends BaseSearchClient<ListUsers> {
 
-    public UsersSearchClient(Context context, String query) {
-        super(context, query);
-    }
+	public UsersSearchClient(Context context, String query) {
+		super(context, query);
+	}
 
-    public UsersSearchClient(Context context, String query, int page) {
-        super(context, query, page);
-    }
+	public UsersSearchClient(Context context, String query, int page) {
+		super(context, query, page);
+	}
 
-    @Override
-    protected void executeFirst(SearchClient searchClient, String query) {
-        searchClient.users(query, this);
-    }
+	@Override
+	protected void executeFirst(SearchClient searchClient, String query) {
+		searchClient.users(query, new Callbacks(this));
+	}
 
-    @Override
-    protected void executePaginated(SearchClient searchClient, String query, int page) {
+	@Override
+	protected void executePaginated(SearchClient searchClient, String query, int page) {
+		searchClient.usersPaginated(query, page, new Callbacks(this));
+	}
 
-    }
+	public class Callbacks implements Callback<UsersSearch> {
+
+		private Callback<ListUsers> callback;
+
+		public Callbacks(Callback<ListUsers> callback) {
+			this.callback = callback;
+		}
+
+		@Override
+		public void success(UsersSearch usersSearch, Response response) {
+			callback.success(usersSearch.items, response);
+		}
+
+		@Override
+		public void failure(RetrofitError error) {
+			callback.failure(error);
+		}
+	}
+
 
 }
