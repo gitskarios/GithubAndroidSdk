7812becf9d020b35b852f9e11a50ec0e7533d280
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-03-09 21:16:17 +0100

    Commit filename

--- src/main/java/com/alorma/github/sdk/bean/dto/response/CommitFile.java
+++ src/main/java/com/alorma/github/sdk/bean/dto/response/CommitFile.java
@@ -1,5 +1,7 @@
 package com.alorma.github.sdk.bean.dto.response;
 
+import java.io.File;
+
 /**
  * Created by Bernat on 22/12/2014.
  */
@@ -10,4 +12,18 @@ public class CommitFile extends GitChangeStatus{
 	public String raw_url;
 	public String blob_url;
 	public String patch;
+
+	public String getFileName() {
+		if (filename != null) {
+			String[] names = filename.split(File.separator);
+			if (names.length > 1) {
+				int last = names.length - 1;
+				return names[last];
+			} else {
+				return names[0];
+			}
+		}
+
+		return null;
+	}
 }
