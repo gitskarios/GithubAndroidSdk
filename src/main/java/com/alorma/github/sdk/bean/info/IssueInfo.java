9939baf12b3415869450894168bda97f2d97e686
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-04-27 01:17:23 +0200

    Open issues from webview

--- src/main/java/com/alorma/github/sdk/bean/info/IssueInfo.java
+++ src/main/java/com/alorma/github/sdk/bean/info/IssueInfo.java
@@ -1,5 +1,6 @@
 package com.alorma.github.sdk.bean.info;
 
+import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
 
@@ -10,53 +11,63 @@ import com.alorma.github.sdk.bean.dto.response.IssueState;
  */
 public class IssueInfo implements Parcelable {
 
-	public RepoInfo repo;
-	public int num;
-	public IssueState state = IssueState.open;
+    public RepoInfo repo;
+    public int num;
+    public IssueState state = IssueState.open;
 
-	public IssueInfo() {
-		
-	}
+    public IssueInfo() {
 
-	public IssueInfo(RepoInfo repoInfo) {
-		this.repo = repoInfo;
-	}
+    }
 
-	protected IssueInfo(Parcel in) {
-		repo = (RepoInfo) in.readValue(RepoInfo.class.getClassLoader());
-		num = in.readInt();
-		int stateValue = in.readInt();
-		state = IssueState.fromValue(stateValue);
+    public IssueInfo(String url) {
+        if (url != null) {
+            repo = new RepoInfo();
+            Uri uri = Uri.parse(url);
+            repo.owner = uri.getPathSegments().get(0);
+            repo.name = uri.getPathSegments().get(1);
+            num = Integer.valueOf(uri.getLastPathSegment());
+        }
+    }
 
-	}
+    public IssueInfo(RepoInfo repoInfo) {
+        this.repo = repoInfo;
+    }
 
-	@Override
-	public int describeContents() {
-		return 0;
-	}
+    protected IssueInfo(Parcel in) {
+        repo = (RepoInfo) in.readValue(RepoInfo.class.getClassLoader());
+        num = in.readInt();
+        int stateValue = in.readInt();
+        state = IssueState.fromValue(stateValue);
 
-	@Override
-	public void writeToParcel(Parcel dest, int flags) {
-		dest.writeValue(repo);
-		dest.writeInt(num);
-		dest.writeInt(state.value);
-	}
+    }
 
-	@SuppressWarnings("unused")
-	public static final Parcelable.Creator<IssueInfo> CREATOR = new Parcelable.Creator<IssueInfo>() {
-		@Override
-		public IssueInfo createFromParcel(Parcel in) {
-			return new IssueInfo(in);
-		}
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
-		@Override
-		public IssueInfo[] newArray(int size) {
-			return new IssueInfo[size];
-		}
-	};
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(repo);
+        dest.writeInt(num);
+        dest.writeInt(state.value);
+    }
 
-	@Override
-	public String toString() {
-		return repo.toString() + " #" + num;
-	}
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<IssueInfo> CREATOR = new Parcelable.Creator<IssueInfo>() {
+        @Override
+        public IssueInfo createFromParcel(Parcel in) {
+            return new IssueInfo(in);
+        }
+
+        @Override
+        public IssueInfo[] newArray(int size) {
+            return new IssueInfo[size];
+        }
+    };
+
+    @Override
+    public String toString() {
+        return repo.toString() + " #" + num;
+    }
 }
