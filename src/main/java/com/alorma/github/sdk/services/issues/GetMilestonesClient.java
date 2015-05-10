3086c86e77a6d3c7df5df2308d745bb3036a0439
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-05-10 21:34:04 +0200

    Get milestones by repo info

--- src/main/java/com/alorma/github/sdk/services/issues/GetMilestonesClient.java
+++ src/main/java/com/alorma/github/sdk/services/issues/GetMilestonesClient.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.alorma.github.sdk.bean.dto.response.Milestone;
 import com.alorma.github.sdk.bean.info.IssueInfo;
+import com.alorma.github.sdk.bean.info.RepoInfo;
 import com.alorma.github.sdk.services.client.GithubClient;
 
 import java.util.ArrayList;
@@ -16,39 +17,39 @@ import retrofit.RestAdapter;
  */
 public class GetMilestonesClient extends GithubClient<List<Milestone>> {
 
-    private IssueInfo issueInfo;
+    private RepoInfo repoInfo;
 
-    public GetMilestonesClient(Context context, IssueInfo issueInfo) {
+    public GetMilestonesClient(Context context, RepoInfo repoInfo) {
         super(context);
-        this.issueInfo = issueInfo;
+        this.repoInfo = repoInfo;
     }
 
     @Override
     protected void executeService(RestAdapter restAdapter) {
         IssuesService issueStoryService = restAdapter.create(IssuesService.class);
-        new IssueMilestonesCallback(issueInfo, issueStoryService).execute();
+        new IssueMilestonesCallback(repoInfo, issueStoryService).execute();
     }
 
     private class IssueMilestonesCallback extends BaseInfiniteCallback<List<Milestone>> {
 
         private List<Milestone> milestones;
-        private IssueInfo issueInfo;
+        private RepoInfo repoInfo;
         private IssuesService service;
 
-        public IssueMilestonesCallback(IssueInfo issueInfo, IssuesService service) {
-            this.issueInfo = issueInfo;
+        public IssueMilestonesCallback(RepoInfo repoInfo, IssuesService service) {
+            this.repoInfo = repoInfo;
             this.service = service;
             milestones = new ArrayList<>();
         }
 
         @Override
         public void execute() {
-            service.milestones(issueInfo.repo.owner, issueInfo.repo.name, this);
+            service.milestones(repoInfo.owner, repoInfo.name, this);
         }
 
         @Override
         protected void executePaginated(int nextPage) {
-            service.milestones(issueInfo.repo.owner, issueInfo.repo.name, nextPage, this);
+            service.milestones(repoInfo.owner, repoInfo.name, nextPage, this);
         }
 
         @Override
