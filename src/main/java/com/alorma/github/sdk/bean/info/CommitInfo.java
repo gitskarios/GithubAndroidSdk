e4c9a75489e05864789453e0fe3d81be1884b4d0
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-05-24 13:11:14 +0200

    Manage URI commit

---
+++ src/main/java/com/alorma/github/sdk/bean/info/CommitInfo.java
@@ -0,0 +1,45 @@
+package com.alorma.github.sdk.bean.info;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Bernat on 24/05/2015.
+ */
+public class CommitInfo implements Parcelable {
+    public RepoInfo repoInfo;
+    public String sha;
+
+    public CommitInfo() {
+
+    }
+
+    protected CommitInfo(Parcel in) {
+        repoInfo = (RepoInfo) in.readValue(RepoInfo.class.getClassLoader());
+        sha = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(repoInfo);
+        dest.writeString(sha);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<CommitInfo> CREATOR = new Parcelable.Creator<CommitInfo>() {
+        @Override
+        public CommitInfo createFromParcel(Parcel in) {
+            return new CommitInfo(in);
+        }
+
+        @Override
+        public CommitInfo[] newArray(int size) {
+            return new CommitInfo[size];
+        }
+    };
+}
--- src/main/java/com/alorma/github/sdk/services/commit/GetSingleCommitClient.java
+++ src/main/java/com/alorma/github/sdk/services/commit/GetSingleCommitClient.java
@@ -3,6 +3,7 @@ package com.alorma.github.sdk.services.commit;
 import android.content.Context;
 
 import com.alorma.github.sdk.bean.dto.response.Commit;
+import com.alorma.github.sdk.bean.info.CommitInfo;
 import com.alorma.github.sdk.bean.info.RepoInfo;
 import com.alorma.github.sdk.services.client.GithubClient;
 
@@ -12,17 +13,16 @@ import retrofit.RestAdapter;
  * Created by Bernat on 22/12/2014.
  */
 public class GetSingleCommitClient extends GithubClient<Commit> {
-	private RepoInfo info;
-	private String sha;
 
-	public GetSingleCommitClient(Context context, RepoInfo info, String sha) {
+	private CommitInfo info;
+
+	public GetSingleCommitClient(Context context, CommitInfo info) {
 		super(context);
 		this.info = info;
-		this.sha = sha;
 	}
 
 	@Override
 	protected void executeService(RestAdapter restAdapter) {
-		restAdapter.create(CommitsService.class).singleCommit(info.owner, info.name, sha, this);
+		restAdapter.create(CommitsService.class).singleCommit(info.repoInfo.owner, info.repoInfo.name, info.sha, this);
 	}
 }
