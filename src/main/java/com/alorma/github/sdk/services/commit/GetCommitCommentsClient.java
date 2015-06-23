fd10cc004af345e3565618f8bb083e998cca9b87
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-06-23 21:05:18 +0200

    Show commit comments

--- src/main/java/com/alorma/github/sdk/services/commit/GetCommitCommentsClient.java
+++ src/main/java/com/alorma/github/sdk/services/commit/GetCommitCommentsClient.java
@@ -15,16 +15,16 @@ import retrofit.client.Response;
 /**
  * Created by Bernat on 22/12/2014.
  */
-public class GetSingleCommitCommentsClient extends GithubClient<List<CommitComment>> {
+public class GetCommitCommentsClient extends GithubClient<List<CommitComment>> {
 
 	private CommitInfo info;
 	private int page = 0;
 
-	public GetSingleCommitCommentsClient(Context context, CommitInfo info) {
+	public GetCommitCommentsClient(Context context, CommitInfo info) {
 		super(context);
 		this.info = info;
 	}
-	public GetSingleCommitCommentsClient(Context context, CommitInfo info, int page) {
+	public GetCommitCommentsClient(Context context, CommitInfo info, int page) {
 		super(context);
 		this.info = info;
 		this.page = page;
@@ -37,5 +37,11 @@ public class GetSingleCommitCommentsClient extends GithubClient<List<CommitComme
 		} else {
 			restAdapter.create(CommitsService.class).singleCommitComments(info.repoInfo.owner, info.repoInfo.name, info.sha, page, this);
 		}
+	}
+
+
+	@Override
+	public String getAcceptHeader() {
+		return "application/vnd.github.v3.html+json";
 	}
 }
