7976138893e58dc6ce2fde94c176029e31286e30
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-06-27 18:47:15 +0200

    Add base sdk classes

---
+++ src/main/java/com/alorma/github/basesdk/ApiClient.java
@@ -0,0 +1,15 @@
+package com.alorma.github.basesdk;
+
+/**
+ * Created by Bernat on 19/04/2015.
+ */
+public interface ApiClient {
+
+    String getApiClient();
+    String getAPiSecret();
+    String getApiOauth();
+    String getApiOauthUrlEndpoint();
+    String getApiEndpoint();
+    String getType();
+
+}
