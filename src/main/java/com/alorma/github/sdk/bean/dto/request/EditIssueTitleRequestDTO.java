63fbfd952dd4c7d57ea93357ad410b5b2af2fefc
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-07-11 20:28:00 +0200

    Change title of issue

---
+++ src/main/java/com/alorma/github/sdk/bean/dto/request/EditIssueTitleRequestDTO.java
@@ -0,0 +1,8 @@
+package com.alorma.github.sdk.bean.dto.request;
+
+/**
+ * Created by Bernat on 17/05/2015.
+ */
+public class EditIssueTitleRequestDTO extends EditIssueRequestDTO {
+    public String title;
+}
