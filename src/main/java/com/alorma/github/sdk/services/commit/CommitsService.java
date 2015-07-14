6a8f702cda67c2b79a2054a06da373b1d4e0d00c
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-07-14 20:16:01 +0200

    History from source

--- src/main/java/com/alorma/github/sdk/services/commit/CommitsService.java
+++ src/main/java/com/alorma/github/sdk/services/commit/CommitsService.java
@@ -15,25 +15,38 @@ import retrofit.http.Query;
  */
 public interface CommitsService {
 
-	@GET("/repos/{owner}/{name}/commits")
-	void commits(@Path("owner") String owner, @Path("name") String repo, Callback<List<Commit>> callback);
+    @GET("/repos/{owner}/{name}/commits/{sha}")
+    void singleCommit(@Path("owner") String owner, @Path("name") String repo, @Path("sha") String sha, Callback<Commit> callback);
 
-	@GET("/repos/{owner}/{name}/commits/{sha}")
-	void singleCommit(@Path("owner") String owner, @Path("name") String repo, @Path("sha") String sha, Callback<Commit> callback);
+    @GET("/repos/{owner}/{name}/commits/{sha}/comments")
+    void singleCommitComments(@Path("owner") String owner, @Path("name") String repo, @Path("sha") String sha, Callback<List<CommitComment>> callback);
 
-	@GET("/repos/{owner}/{name}/commits/{sha}/comments")
-	void singleCommitComments(@Path("owner") String owner, @Path("name") String repo, @Path("sha") String sha, Callback<List<CommitComment>> callback);
+    @GET("/repos/{owner}/{name}/commits/{sha}/comments")
+    void singleCommitComments(@Path("owner") String owner, @Path("name") String repo, @Path("sha") String sha, @Query("page") int page, Callback<List<CommitComment>> callback);
 
-	@GET("/repos/{owner}/{name}/commits/{sha}/comments")
-	void singleCommitComments(@Path("owner") String owner, @Path("name") String repo, @Path("sha") String sha, @Query("page") int page, Callback<List<CommitComment>> callback);
 
-	@GET("/repos/{owner}/{name}/commits")
-	void commits(@Path("owner") String owner, @Path("name") String repo, @Query("page") int page, Callback<List<Commit>> callback);
+    @GET("/repos/{owner}/{name}/commits")
+    void commits(@Path("owner") String owner, @Path("name") String repo, Callback<List<Commit>> callback);
 
-	@GET("/repos/{owner}/{name}/commits")
-	void commits(@Path("owner") String owner, @Path("name") String repo, @Query("sha") String sha, Callback<List<Commit>> callback);
+    @GET("/repos/{owner}/{name}/commits")
+    void commits(@Path("owner") String owner, @Path("name") String repo, @Query("page") int page, Callback<List<Commit>> callback);
 
-	@GET("/repos/{owner}/{name}/commits")
-	void commits(@Path("owner") String owner, @Path("name") String repo, @Query("page") int page, @Query("sha") String sha, Callback<List<Commit>> callback);
+    @GET("/repos/{owner}/{name}/commits")
+    void commits(@Path("owner") String owner, @Path("name") String repo, @Query("sha") String sha, Callback<List<Commit>> callback);
+
+    @GET("/repos/{owner}/{name}/commits")
+    void commits(@Path("owner") String owner, @Path("name") String repo, @Query("page") int page, @Query("sha") String sha, Callback<List<Commit>> callback);
+
+    @GET("/repos/{owner}/{name}/commits")
+    void commitsByPath(@Path("owner") String owner, @Path("name") String repo, @Query("path") String path, Callback<List<Commit>> callback);
+
+    @GET("/repos/{owner}/{name}/commits")
+    void commitsByPath(@Path("owner") String owner, @Path("name") String repo, @Query("path") String path, @Query("page") int page, Callback<List<Commit>> callback);
+
+    @GET("/repos/{owner}/{name}/commits")
+    void commitsByPath(@Path("owner") String owner, @Path("name") String repo, @Query("path") String path, @Query("sha") String sha, Callback<List<Commit>> callback);
+
+    @GET("/repos/{owner}/{name}/commits")
+    void commitsByPath(@Path("owner") String owner, @Path("name") String repo, @Query("path") String path, @Query("sha") String sha, @Query("page") int page, Callback<List<Commit>> callback);
 
 }
--- src/main/java/com/alorma/github/sdk/services/commit/ListCommitsClient.java
+++ src/main/java/com/alorma/github/sdk/services/commit/ListCommitsClient.java
@@ -14,30 +14,54 @@ import retrofit.RestAdapter;
  * Created by Bernat on 07/09/2014.
  */
 public class ListCommitsClient extends GithubClient<List<Commit>> {
-	private RepoInfo info;
-	private int page;
+    private RepoInfo info;
+    private String path;
+    private int page;
 
-	public ListCommitsClient(Context context, RepoInfo info, int page) {
-		super(context);
-		this.info = info;
-		this.page = page;
-	}
+    public ListCommitsClient(Context context, RepoInfo info, int page) {
+        super(context);
+        this.info = info;
+        this.page = page;
+    }
 
-	@Override
-	protected void executeService(RestAdapter restAdapter) {
-		CommitsService commitsService = restAdapter.create(CommitsService.class);
-		if (info.branch == null) {
-			if (page == 0) {
-				commitsService.commits(info.owner, info.name, this);
-			} else {
-				commitsService.commits(info.owner, info.name, page, this);
-			}
-		} else {
-			if (page == 0) {
-				commitsService.commits(info.owner, info.name, info.branch, this);
-			} else {
-				commitsService.commits(info.owner, info.name, page, info.branch, this);
-			}
-		}
-	}
+    public ListCommitsClient(Context context, RepoInfo info, String path, int page) {
+        super(context);
+        this.info = info;
+        this.path = path;
+        this.page = page;
+    }
+
+    @Override
+    protected void executeService(RestAdapter restAdapter) {
+        CommitsService commitsService = restAdapter.create(CommitsService.class);
+        if (path == null) {
+            if (info.branch == null) {
+                if (page == 0) {
+                    commitsService.commits(info.owner, info.name, this);
+                } else {
+                    commitsService.commits(info.owner, info.name, page, this);
+                }
+            } else {
+                if (page == 0) {
+                    commitsService.commits(info.owner, info.name, info.branch, this);
+                } else {
+                    commitsService.commits(info.owner, info.name, page, info.branch, this);
+                }
+            }
+        } else {
+            if (info.branch == null) {
+                if (page == 0) {
+                    commitsService.commitsByPath(info.owner, info.name, path, this);
+                } else {
+                    commitsService.commitsByPath(info.owner, info.name, path, page, this);
+                }
+            } else {
+                if (page == 0) {
+                    commitsService.commitsByPath(info.owner, info.name, path, info.branch, this);
+                } else {
+                    commitsService.commitsByPath(info.owner, info.name, path, info.branch, page, this);
+                }
+            }
+        }
+    }
 }
