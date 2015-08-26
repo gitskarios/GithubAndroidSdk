c65f0131ba9fa26943d2b3f9f0cec1a1e3d823fc
Henrik <henrikolsson@outlook.com>
2015-08-26 21:52:34 +0200

    Added GitReference-, GitCommit-, GitTree- and GitBlobClient

--- src/main/java/com/alorma/github/sdk/services/git/GitDataService.java
+++ src/main/java/com/alorma/github/sdk/services/git/GitDataService.java
@@ -1,10 +1,14 @@
 package com.alorma.github.sdk.services.git;
 
+import com.alorma.github.sdk.bean.dto.response.GitBlob;
+import com.alorma.github.sdk.bean.dto.response.GitCommit;
 import com.alorma.github.sdk.bean.dto.response.GitReference;
+import com.alorma.github.sdk.bean.dto.response.GitTree;
 
 import java.util.List;
 
 import retrofit.Callback;
+import retrofit.http.EncodedPath;
 import retrofit.http.GET;
 import retrofit.http.Path;
 import retrofit.http.Query;
@@ -20,7 +24,19 @@ public interface GitDataService {
     void repoReferences(@Path("owner") String owner, @Path("repo") String name, @Query("page") int page, Callback<List<GitReference>> callback);
 
     @GET("/repos/{owner}/{repo}/git/{ref}")
-    void repoReference(@Path("owner") String owner, @Path("repo") String name, @Path("ref") String ref, Callback<GitReference> callback);
+    void repoReference(@Path("owner") String owner, @Path("repo") String name, @Path(value = "ref", encode = false) String ref, Callback<GitReference> callback);
+
+    @GET("/repos/{owner}/{repo}/git/commits/{sha}")
+    void repoCommit(@Path("owner") String owner, @Path("repo") String name, @Path("sha") String sha, Callback<GitCommit> callback);
+
+    @GET("/repos/{owner}/{repo}/git/trees/{sha}")
+    void repoTree(@Path("owner") String owner, @Path("repo") String name, @Path("sha") String sha, Callback<GitTree> callback);
+
+    @GET("/repos/{owner}/{repo}/git/trees/{sha}?recursive=1")
+    void repoTreeRecursive(@Path("owner") String owner, @Path("repo") String name, @Path("sha") String sha, Callback<GitTree> callback);
+
+    @GET("/repos/{owner}/{repo}/git/blobs/{sha}")
+    void repoBlob(@Path("owner") String owner, @Path("repo") String name, @Path("sha") String sha, Callback<GitBlob> callback);
 
     //Sync
     @GET("/repos/{owner}/{repo}/git/refs")
@@ -30,5 +46,17 @@ public interface GitDataService {
     List<GitReference> repoReferences(@Path("owner") String owner, @Path("repo") String name, @Query("page") int page);
 
     @GET("/repos/{owner}/{repo}/git/{ref}")
-    GitReference repoReference(@Path("owner") String owner, @Path("repo") String name, @Path("ref") String ref);
+    GitReference repoReference(@Path("owner") String owner, @Path("repo") String name,  @Path(value = "ref", encode = false) String ref);
+
+    @GET("/repos/{owner}/{repo}/git/commits/{sha}")
+    GitCommit repoCommit(@Path("owner") String owner, @Path("repo") String name, @Path("sha") String sha);
+
+    @GET("/repos/{owner}/{repo}/git/trees/{sha}")
+    GitTree repoTree(@Path("owner") String owner, @Path("repo") String name, @Path("sha") String sha);
+
+    @GET("/repos/{owner}/{repo}/git/trees/{sha}?recursive=1")
+    GitTree repoTreeRecursive(@Path("owner") String owner, @Path("repo") String name, @Path("sha") String sha);
+
+    @GET("/repos/{owner}/{repo}/git/blobs/{sha}")
+    GitBlob repoBlob(@Path("owner") String owner, @Path("repo") String name, @Path("sha") String sha);
 }
