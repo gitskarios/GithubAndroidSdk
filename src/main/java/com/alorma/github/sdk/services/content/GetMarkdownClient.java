e3178e9b2f7f4e2c44f6bb7b19dca11c6bbf2f0e
Bernat Borrás Paronella <bernat.borras@worldline.com>
2015-08-29 11:24:41 +0200

    Update latest development tools

--- src/main/java/com/alorma/github/sdk/services/content/GetMarkdownClient.java
+++ src/main/java/com/alorma/github/sdk/services/content/GetMarkdownClient.java
@@ -1,30 +1,15 @@
 package com.alorma.github.sdk.services.content;
 
 import android.content.Context;
-import android.os.Handler;
 
-import com.alorma.github.basesdk.client.StoreCredentials;
 import com.alorma.github.sdk.bean.dto.request.RequestMarkdownDTO;
-import com.alorma.github.sdk.security.GitHub;
 import com.alorma.github.sdk.services.client.GithubClient;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Scanner;
 
-import retrofit.Callback;
-import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.Client;
-import retrofit.client.Request;
-import retrofit.client.Response;
 import retrofit.converter.ConversionException;
 import retrofit.converter.Converter;
 import retrofit.mime.TypedInput;
