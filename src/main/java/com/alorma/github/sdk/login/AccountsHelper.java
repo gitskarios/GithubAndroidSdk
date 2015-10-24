d72b61378b9986b4f830de32a2f9867f9f7becee
Bernat Borrás Paronella <bernat.borras@scmspain.com>
2015-10-24 13:27:15 +0200

    Login working

--- src/main/java/com/alorma/github/sdk/login/AccountsHelper.java
+++ src/main/java/com/alorma/github/sdk/login/AccountsHelper.java
@@ -9,16 +9,14 @@ import android.os.Bundle;
  * Created by Bernat on 02/04/2015.
  */
 public class AccountsHelper {
-    private static final String ACCOUNT_SCOPES = "ACCOUNT_SCOPES";
     public static final String USER_PIC = "USER_PIC";
     private static final String USER_MAIL = "USER_MAIL";
     private static final String USER_NAME = "USER_NAME";
     public static final String USER_URL = "USER_URL";
 
-    public static Bundle buildBundle(String name, String mail, String avatar, String scope) {
+    public static Bundle buildBundle(String name, String mail, String avatar) {
         Bundle userData = new Bundle();
 
-        userData.putString(AccountsHelper.ACCOUNT_SCOPES, scope);
         userData.putString(AccountsHelper.USER_PIC, avatar);
         userData.putString(AccountsHelper.USER_MAIL, mail);
         userData.putString(AccountsHelper.USER_NAME, name);
@@ -46,10 +44,6 @@ public class AccountsHelper {
         return manager.getUserData(account, USER_NAME);
     }
 
-    public static String getUserScopes(Context context, Account account) {
-        AccountManager manager = AccountManager.get(context);
-        return manager.getUserData(account, ACCOUNT_SCOPES);
-    }
     public static String getUserToken(Context context, Account account) {
         AccountManager manager = AccountManager.get(context);
         return manager.getUserData(account, AccountManager.KEY_AUTHTOKEN);
