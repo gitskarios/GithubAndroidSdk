be213f3860351408c243e7b5730c965ef369ddfb
Bernat Borrás Paronella <bernatbor15@gmail.com>
2016-04-21 23:03:53 +0200

    moving to reborn

--- src/main/java/com/alorma/github/sdk/bean/dto/response/Branch.java
+++ src/main/java/com/alorma/github/sdk/bean/dto/response/Branch.java
@@ -3,9 +3,6 @@ package com.alorma.github.sdk.bean.dto.response;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-/**
- * Created by Bernat on 20/07/2014.
- */
 public class Branch implements Parcelable {
   public static final Creator<Branch> CREATOR = new Creator<Branch>() {
     public Branch createFromParcel(Parcel source) {
